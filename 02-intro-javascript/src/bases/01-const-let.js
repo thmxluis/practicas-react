bb733d296390024116c35faf398edd48fb40967b
Luis Gonzalez <lgonzalez@talana.com>
2023-11-26 10:48:08 -0500

    Agregados submódulos 01-intro-react y 02-intro-javascript

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,17 @@
+// Variables y constantes
+
+const nombre = 'Luis';
+const apellido = 'Gonzalez';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado); 
+
+
+if (true) {
+    const nombre = 'Peter';
+    console.log(nombre);
+}
+
+console.log(nombre);
